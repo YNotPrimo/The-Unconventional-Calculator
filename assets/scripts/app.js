a63b044432cb3f12387df6b45c4a26a9da32e31f
Primo <primo@pop-os.localdomain>
2020-08-19 19:08:23 +0300

    Refactored

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
+const CALCULATION_TYPE_ADD = "ADD";
+const CALCULATION_TYPE_SUBTRACT = "SUBTRACT";
+const CALCULATION_TYPE_MULTIPLY = "MULTIPLY";
+const CALCULATION_TYPE_DIVIDE = "DIVIDE";
 
 // Gets input from the input field
 function getUserInput() {
@@ -30,12 +34,12 @@ function writeToLog(
 
 function calculateResult(calculationType) {
   const enteredNumber = getUserInput();
-  
+
   if (
-    (calculationType !== "ADD" &&
-      calculationType !== "SUBTRACT" &&
-      calculationType !== "MULTIPLY" &&
-      calculationType !== "DIVIDE") ||
+    (calculationType !== CALCULATION_TYPE_ADD &&
+      calculationType !== CALCULATION_TYPE_SUBTRACT &&
+      calculationType !== CALCULATION_TYPE_MULTIPLY &&
+      calculationType !== CALCULATION_TYPE_DIVIDE) ||
     !enteredNumber
   ) {
     return;
@@ -44,16 +48,16 @@ function calculateResult(calculationType) {
   const initialResult = currentResult;
   let mathOperator;
 
-  if (calculationType === "ADD") {
+  if (calculationType === CALCULATION_TYPE_ADD) {
     currentResult += enteredNumber;
     mathOperator = "+";
-  } else if (calculationType === "SUBTRACT") {
+  } else if (calculationType === CALCULATION_TYPE_SUBTRACT) {
     currentResult -= enteredNumber;
     mathOperator = "-";
-  } else if (calculationType === "MULTIPLY") {
+  } else if (calculationType === CALCULATION_TYPE_MULTIPLY) {
     currentResult *= enteredNumber;
     mathOperator = "*";
-  } else if (calculationType === "DIVIDE") {
+  } else if (calculationType === CALCULATION_TYPE_DIVIDE) {
     currentResult /= enteredNumber;
     mathOperator = "/";
   }
@@ -61,24 +65,32 @@ function calculateResult(calculationType) {
   createAndWriteLog(mathOperator, initialResult, enteredNumber);
   writeToLog(calculateResult, initialResult, enteredNumber, currentResult);
 }
-
-function add() {
-  calculateResult("ADD");
+function calculate(operation) {
+  switch (operation) {
+    case CALCULATION_TYPE_ADD:
+      calculateResult(CALCULATION_TYPE_ADD);
+      break;
+    case CALCULATION_TYPE_SUBTRACT:
+      calculateResult(CALCULATION_TYPE_SUBTRACT);
+      break;
+    case CALCULATION_TYPE_MULTIPLY:
+      calculateResult(CALCULATION_TYPE_MULTIPLY);
+      break;
+    case CALCULATION_TYPE_DIVIDE:
+      calculateResult(CALCULATION_TYPE_DIVIDE);
+      break;
+  }
 }
-
-function subtract() {
-  calculateResult("SUBTRACT");
-}
-
-function multiply() {
-  calculateResult("MULTIPLY");
-}
-
-function divide() {
-  calculateResult("DIVIDE");
-}
-
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+addBtn.addEventListener("click", calculate.bind(this, CALCULATION_TYPE_ADD));
+subtractBtn.addEventListener(
+  "click",
+  calculate.bind(this, CALCULATION_TYPE_SUBTRACT)
+);
+multiplyBtn.addEventListener(
+  "click",
+  calculate.bind(this, CALCULATION_TYPE_MULTIPLY)
+);
+divideBtn.addEventListener(
+  "click",
+  calculate.bind(this, CALCULATION_TYPE_DIVIDE)
+);
